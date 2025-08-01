b205f3e263686674414dd333900aa25969e4f07a
Jyry Niemitalo <jyniemit@student.hive.fi>
2025-08-01 19:48:54 +0300

    WIP(env): environment initializer, does not feel good as it is.
    I believe we really do need the arena allocator after all. There's
    already spaghetti code in this, that could all be cleared up with
    allocated arena. There's already multiple mallocs with the ft_strdup
    here, and it does not feel good to get this function under 25 lines just
    by accident.

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: jyniemit <jyniemit@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/29 14:50:29 by jyniemit          #+#    #+#             */
-/*   Updated: 2025/08/18 15:33:43 by jyniemit         ###   ########.fr       */
+/*   Updated: 2025/08/18 15:44:46 by jyniemit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,35 @@ int	main(int ac, char **av)
 	init_shell(ac, av, &shell);
 	run_shell(&shell);
 	return (shell.code);
+}
+
+int		init_env(t_shell *shell)
+{
+	extern char	**environ;
+	int			i;
+	
+	i = -1;
+	shell->original_env = environ;
+	while (environ[shell->env_count])
+		shell->env_count++;
+	shell->capacity = shell->env_count + 10;
+	shell->heap_env = malloc(sizeof(char *) * (shell->capacity + 1))
+	if(!shell->heap_env)
+		return (0);
+	while (++i < shell->env_count)
+	{
+		shell->heap_env[i] = ft_strdup(environ[i]);
+		if (!shell->heap_env)
+		{
+			while (--i >= 0)
+				free(shell->heap_env);
+			free(shell->heap_env);
+			shell->heap_env = NULL;
+			return (0);
+		}
+	}
+	shell->heap_env[shell->env_count] = NULL;
+	return (1);
 }
 
 void	init_shell(int ac, char **av, t_shell *shell)
