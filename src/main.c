b5736101635434a771e910e009b6c665fa85670b
Jyry Niemitalo <jyniemit@student.hive.fi>
2025-08-21 12:59:28 +0300

    fix(env): using arena for env allocation

--- src/main.c
+++ src/main.c
@@ -6,19 +6,19 @@
 /*   By: jyniemit <jyniemit@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/29 14:50:29 by jyniemit          #+#    #+#             */
-/*   Updated: 2025/08/19 12:22:46 by jyniemit         ###   ########.fr       */
+/*   Updated: 2025/08/21 12:37:20 by jyniemit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	main(int ac, char **av)
+int	main(int ac, char **av, char **env)
 {
 	t_shell	shell;
 
 	init_signals(&shell);
 	ft_bzero(&shell, sizeof(t_shell));
-	init_shell(ac, av, &shell);
+	init_shell(ac, av, env, &shell);
 	run_shell(&shell);
 	return (shell.code);
 }
--- src/repl.c
+++ src/repl.c
@@ -6,42 +6,28 @@
 /*   By: jyniemit <jyniemit@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/29 12:37:53 by jyniemit          #+#    #+#             */
-/*   Updated: 2025/08/20 13:55:46 by jyniemit         ###   ########.fr       */
+/*   Updated: 2025/08/21 12:55:17 by jyniemit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int		init_env(t_shell *shell)
+static void	init_env(char **env, t_shell *shell)
 {
-	extern char	**environ;
-	int			i;
+	int	i;
 	
 	i = -1;
-	shell->original_env = environ;
-	while (environ[shell->env_count])
+	shell->original_env = env;
+	while (env[shell->env_count])
 		shell->env_count++;
 	shell->env_capacity = shell->env_count + 10;
 	shell->heap_env = (char *)s_malloc(sizeof(char *) * (shell->env_capacity + 1));
-	if(!shell->heap_env)
-		return (0);
 	while (++i < shell->env_count)
-	{
-		shell->heap_env[i] = ft_strdup(environ[i]);
-		if (!shell->heap_env)
-		{
-			while (--i >= 0)
-				free(shell->heap_env);
-			free(shell->heap_env);
-			shell->heap_env = NULL;
-			return (0);
-		}
-	}
+		shell->heap_env[i] = a_strdup(env[i]);
 	shell->heap_env[shell->env_count] = NULL;
-	return (1);
 }
 
-void	init_shell(int ac, char **av, t_shell *shell)
+void	init_shell(int ac, char **av, char **env, t_shell *shell)
 {
 	(void)ac;
 	(void)av;
@@ -55,7 +41,8 @@ void	init_shell(int ac, char **av, t_shell *shell)
 	shell->pipe_read_fd = -1;
 	shell->pipe_write_fd = -1;
 	shell->token_count = 0;
-	if (!getcwd(shell->working_directory, PATH_MAX) || !init_env(shell))
+	init_env(env, shell);
+	if (!getcwd(shell->working_directory, PATH_MAX))
 	{
 		shell->code = EXIT_SHELLINITFAIL;
 		shell->state |= SHOULD_EXIT;
