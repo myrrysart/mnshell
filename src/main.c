cb2aac17d6e7a858d02febcba840c58ebebdbd38
Jyry Niemitalo <jyniemit@student.hive.fi>
2025-08-26 14:20:02 +0300

    feat(env): env called trough main, saved to the fat struct in its entirety, export, unset and get functions added

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: jyniemit <jyniemit@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/29 14:50:29 by jyniemit          #+#    #+#             */
-/*   Updated: 2025/08/21 12:37:20 by jyniemit         ###   ########.fr       */
+/*   Updated: 2025/08/26 12:09:03 by jyniemit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "minishell.h"
@@ -15,9 +15,9 @@ int	main(int ac, char **av, char **env)
 {
 	t_shell	shell;
 
-	init_signals(&shell);
 	ft_bzero(&shell, sizeof(t_shell));
 	init_shell(ac, av, env, &shell);
+	init_signals(&shell);
 	run_shell(&shell);
 	return (shell.code);
 }
