6e7ed9f274c1457da2890d2e3ae98037936bdfb8
Jyry Niemitalo <jyniemit@student.hive.fi>
2025-10-15 18:55:16 +0300

    feat(exit): exit code returns and error checks now work as intended

--- libft/ft_atoi.c
+++ libft/ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: Jyniemit <jyniemit@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/04 12:52:10 by Jyniemit          #+#    #+#             */
-/*   Updated: 2024/11/11 13:26:49 by Jyniemit         ###   ########.fr       */
+/*   Updated: 2025/10/15 18:51:45 by jyniemit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,4 +44,32 @@ int	ft_atoi(const char *str)
 		i++;
 	}
 	return (res * sign);
+}
+
+long long	ft_atol(const char *str)
+{
+	int						i;
+	int						sign;
+	unsigned long long int	raw;
+
+	i = 0;
+	raw = 0;
+	sign = 1;
+	while (ft_iswhitespace((int)str[i]))
+		i++;
+	if (str[i] == '-')
+	{
+		sign = -1;
+		i++;
+	}
+	else if (str[i] == '+')
+		i++;
+	while (ft_isdigit((int)str[i]))
+	{
+		raw = raw * 10 + (str[i] - '0');
+		i++;
+	}
+	if (raw > LLONG_MAX)
+		return (-1);
+	return ((long long int)raw * sign);
 }
--- src/builtin_exit.c
+++ src/builtin_exit.c
@@ -6,24 +6,39 @@
 /*   By: jyniemit <jyniemit@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/02 15:41:54 by jyniemit          #+#    #+#             */
-/*   Updated: 2025/10/09 11:23:34 by jyniemit         ###   ########.fr       */
+/*   Updated: 2025/10/15 18:26:04 by jyniemit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	builtin_exit(t_shell *shell, t_cmd_table *cmd)
+static void	builtin_exit_error(t_shell *shell, char *arg, int code)
 {
-	char	*arg;
-	int		i;
-
-	arg = cmd->cmd_da->items[1];
-	if (cmd->cmd_da->items[2])
+	if (code == 1)
 	{
-		ft_putendl_fd("exit: too many arguments", 2);
-		shell->code = EXIT_GENERAL_ERROR;
+		ft_putstr_fd("exit: ", 2);
+		ft_putstr_fd(arg, 2);
+		ft_putendl_fd(": numeric argument required", 2);
+		shell->code = EXIT_BUILTIN_MISUSE;
+		shell->state |= SHOULD_EXIT;
+		if (!(shell->state & HAS_PIPE))
+			write(STDOUT_FILENO, "exit\n", 5);
 		return ;
 	}
+	if (code == 2)
+	{
+			ft_putendl_fd("exit: too many arguments", 2);
+			shell->code = EXIT_GENERAL_ERROR;
+	}
+}
+
+void	builtin_exit(t_shell *shell, t_cmd_table *cmd)
+{
+	char		*arg;
+	int			i;
+	long long	j;
+
+	arg = cmd->cmd_da->items[1];
 	if (arg)
 	{
 		i = 0;
@@ -33,18 +48,23 @@ void	builtin_exit(t_shell *shell, t_cmd_table *cmd)
 		{
 			if (!ft_isdigit(arg[i]))
 			{
-				ft_putstr_fd("exit: ", 2);
-				ft_putstr_fd(arg, 2);
-				ft_putendl_fd(": numeric argument required", 2);
-				shell->code = EXIT_BUILTIN_MISUSE;
-				shell->state |= SHOULD_EXIT;
-				if (!(shell->state & HAS_PIPE))
-					write(STDOUT_FILENO, "exit\n", 5);
+				builtin_exit_error(shell, arg, 1);
 				return ;
 			}
 			i++;
 		}
-		shell->code = (unsigned char)ft_atoi(arg);
+		if (cmd->cmd_da->items[2])
+		{
+			builtin_exit_error(shell, arg, 2);
+			return ;
+		}
+		j = ft_atol(arg);
+		if (j == -1)
+		{
+			builtin_exit_error(shell, arg, 1);
+			return ;
+		}
+		shell->code = (unsigned char)j;
 	}
 	shell->state |= SHOULD_EXIT;
 	if (!(shell->state & HAS_PIPE) && isatty(STDIN_FILENO))
