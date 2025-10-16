5966b62109ab7ae6626fd6e02a74a8c064c51525
hieutrpham <hieutrungpham91@gmail.com>
2025-10-16 17:19:38 +0300

    refactor(exec): norm execution

--- src/exec_others.c
+++ src/exec_others.c
@@ -6,28 +6,13 @@
 /*   By: jyniemit <jyniemit@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/30 17:23:53 by jyniemit          #+#    #+#             */
-/*   Updated: 2025/10/17 14:53:26 by jyniemit         ###   ########.fr       */
+/*   Updated: 2025/10/17 14:54:36 by jyniemit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	builtin_select(t_shell *shell, t_cmd_table *cmd)
-{
-	const t_builtin	builtin_table[BUILTIN_COUNT] = {
-		builtin_cd,
-		builtin_echo,
-		builtin_exit,
-		builtin_pwd,
-		builtin_export,
-		builtin_unset,
-		builtin_env,
-	};
-
-	builtin_table[cmd->cmd_type](shell, cmd);
-}
-
-void	exec_cleanup_parent(t_cmd_table *cmd)
+static void	exec_cleanup_parent(t_cmd_table *cmd)
 {
 	if (cmd->fd_in != STDIN_FILENO)
 		close(cmd->fd_in);
