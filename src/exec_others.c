afa26819dcc0f803337acd6493ec9d55fb165700
hieutrpham <hieutrungpham91@gmail.com>
2025-10-17 16:32:14 +0300

    refactor(exec): exec_others.c

--- src/exec_others.c
+++ src/exec_others.c
@@ -6,7 +6,7 @@
 /*   By: jyniemit <jyniemit@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/30 17:23:53 by jyniemit          #+#    #+#             */
-/*   Updated: 2025/10/17 15:50:36 by trupham          ###   ########.fr       */
+/*   Updated: 2025/10/17 16:27:55 by trupham          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,60 +20,61 @@ static void	exec_cleanup_parent(t_cmd_table *cmd)
 		close(cmd->fd_out);
 }
 
-void fork_error(t_shell *shell, t_cmd_table *cmd)
+void	fork_error(t_shell *shell, t_cmd_table *cmd)
 {
 	perror("fork");
 	shell->code = errno;
 	exec_cleanup_parent(cmd);
 }
 
-void	exec_no_pipe(t_shell *shell)
+static void	exec_child_prep(t_shell *shell, t_cmd_table *cmd)
 {
-	pid_t		child;
-	t_cmd_table	*cmd;
-	int			status;
 	struct stat	st;
 
-	cmd = shell->cmd;
-	if (cmd->cmd_type != EXTERNAL)
-		return (exec_builtin_with_redirs(shell, cmd));
-	if (!cmd->cmd_da || cmd->cmd_da->count == 0)
+	setup_child_signals();
+	exec_apply_redirs(cmd);
+	if (cmd->cmd_da && cmd->cmd_da->count == 1
+		&& ft_strncmp(cmd->cmd_da->items[0], ".", 2) == 0)
 	{
-		exec_cleanup_parent(cmd);
-		shell->code = 0;
+		ft_putendl_fd(".: filename argument required", 2);
+		child_cleanup_and_exit(shell, cmd, EXIT_BUILTIN_MISUSE);
+	}
+	if (cmd->cmd_da && cmd->cmd_da->items[0]
+		&& stat(cmd->cmd_da->items[0], &st) == 0 && S_ISDIR(st.st_mode))
+	{
+		print_err(cmd->cmd_da->items[0], ": Is a directory");
+		child_cleanup_and_exit(shell, cmd, EXIT_CMD_NOT_EXECUTABLE);
+	}
+	if (!cmd->cmd_da || !cmd->cmd_da->items[0])
+		child_cleanup_and_exit(shell, cmd, OK);
+}
+
+void	exec_no_pipe(t_shell *sh)
+{
+	pid_t		child;
+	int			status;
+
+	if (sh->cmd->cmd_type != EXTERNAL)
+		return (exec_builtin_with_redirs(sh, sh->cmd));
+	if (!sh->cmd->cmd_da || sh->cmd->cmd_da->count == 0)
+	{
+		exec_cleanup_parent(sh->cmd);
+		sh->code = 0;
 		return ;
 	}
 	child = fork();
 	if (child < 0)
-		return (fork_error(shell, cmd));
+		return (fork_error(sh, sh->cmd));
 	if (child == 0)
 	{
-		setup_child_signals();
-		exec_apply_redirs(cmd);
-		if (cmd->cmd_da && cmd->cmd_da->count == 1
-			&& ft_strncmp(cmd->cmd_da->items[0], ".", 2) == 0)
-		{
-			ft_putendl_fd(".: filename argument required", 2);
-			child_cleanup_and_exit(shell, cmd, EXIT_BUILTIN_MISUSE);
-		}
-		if (cmd->cmd_da && cmd->cmd_da->items[0])
-		{
-			if (stat(cmd->cmd_da->items[0], &st) == 0 && S_ISDIR(st.st_mode))
-			{
-				ft_putstr_fd(cmd->cmd_da->items[0], 2);
-				ft_putendl_fd(": Is a directory", 2);
-				child_cleanup_and_exit(shell, cmd, EXIT_CMD_NOT_EXECUTABLE);
-			}
-		}
-		if (!cmd->cmd_da || !cmd->cmd_da->items[0])
-			child_cleanup_and_exit(shell, cmd, OK);
-		execve(cmd->cmd_da->items[0], cmd->cmd_da->items, shell->heap_env);
-		errno_report(cmd);
-		child_cleanup_and_exit(shell, cmd, map_exec_errno_to_exit(errno));
+		exec_child_prep(sh, sh->cmd);
+		execve(sh->cmd->cmd_da->items[0], sh->cmd->cmd_da->items, sh->heap_env);
+		errno_report(sh->cmd);
+		child_cleanup_and_exit(sh, sh->cmd, map_exec_errno_to_exit(errno));
 	}
 	waitpid(child, &status, 0);
-	shell_update_code_from_status(shell, status);
-	exec_cleanup_parent(cmd);
+	shell_update_code_from_status(sh, status);
+	exec_cleanup_parent(sh->cmd);
 }
 
 void	exec_pipeline(t_shell *shell)
